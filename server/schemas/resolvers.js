47aa7cdcba6ce493076983a9812f7e9e8d808b8a
Rona C <rona.chen@hotmail.com>
2023-07-13 16:17:25 -0400

    Reorganized files into the client folder to work with React app. Added base component code ith rafce and removed all errors in client app.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,8 +6,44 @@ const resolvers = {
   Query: {
     user: async (parent, args, context) => {
       if (context.user) {
-        const user = await User.findById(context.user._id).populate();
+        const user = await User.findById(context.user._id).populate('pets');
+        return user;
       }
+      throw new AuthenticationError('Not logged in');
+    },
+    species: async () => {
+      return await Species.find();
     }
+  },
+  Mutation: {
+    // addUser mutation logic
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    // login mutation logic
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // addPet mutation logic
+    addPet: async (parent, { name, species, age, gender, owner }) => {},
+    // updatePet mutation logic
+    updatePet: async (parent, { petID, name }) => {},
+    // deletePet mutation logic
+    deletePet: async (parent, { petID, userID }) => {}
   }
 };
