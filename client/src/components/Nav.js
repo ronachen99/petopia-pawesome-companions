a77029c4d62435a4df187ae11bc900d51d54d622
Andrew <andy.p.123@hotmail.com>
2023-07-14 10:02:26 -0400

    nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
-const Nav = () => {
-  return <div>Nav</div>;
-};
+// const Nav = () => {
+
+//   return <div>Nav</div>;
+// };
+
+// export default Nav;
+function Nav() {
+  if (Auth.loggedIn()) {
+    return (
+      <ul className="flex-row">
+        <li className="mx-1">
+          <Link to="/orderHistory">Order History</Link>
+        </li>
+        <li className="mx-1">
+          {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+          <a href="/" onClick={() => Auth.logout()}>
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="flex-row">
+        <li className="mx-1">
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li className="mx-1">
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    );
+  }
+  return (
+    <header className="flex-row px-1">
+      <h1>
+        <Link to="/">
+          <span role="img" aria-label="shopping bag">
+            🛍️
+          </span>
+          -Shop-Shop
+        </Link>
+      </h1>
+
+      <nav>{Nav()}</nav>
+    </header>
+  );
+}
 
 export default Nav;
